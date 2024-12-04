2aa9cc563bc395259fba8c287e8022cd11218a97
ferenc02 <ferenc.sziraki@medieinstitutet.se>
2024-12-04 22:40:29 +0100

    :sparkles: Fix structure again

---
+++ template.js
@@ -0,0 +1,12 @@
+let canvasWidth = 600;
+let canvasHeight = 600;
+
+function setup() {
+  createCanvas(canvasWidth, canvasHeight);
+}
+
+function draw() {
+  background(0);
+  fill(255);
+  ellipse(width / 2, height / 2, 50, 50);
+}
