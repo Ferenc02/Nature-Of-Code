8c4df8c35d534fe6d615921e3de96f22107e641b
ferenc02 <ferenc.sziraki@medieinstitutet.se>
2024-12-07 13:46:06 +0100

    :sparkles: Enhance Perlin noise visualizer with interactive slider and project description

--- Projects/Perlin-noise/script.js
+++ Projects/Perlin-noise/script.js
@@ -1,12 +1,43 @@
+/* Visual representation of one-dimensional Perlin noise */
+
 let canvasWidth = 600;
 let canvasHeight = 600;
 
+let incrementValue = 0.01;
+let start = 0;
+
 function setup() {
-  createCanvas(canvasWidth, canvasHeight);
+  let canvas = createCanvas(canvasWidth, canvasHeight);
+  canvas.parent("canvas");
 }
 
 function draw() {
-  background(0);
+  background(50);
   fill(255);
-  ellipse(width / 2, height / 2, 50, 50);
+
+  stroke(255);
+  noFill();
+  beginShape();
+
+  let xoff = start;
+  for (let x = 0; x < width; x++) {
+    stroke(255);
+
+    let y = noise(xoff) * height;
+
+    vertex(x, y);
+    xoff += incrementValue;
+  }
+
+  endShape();
+
+  start += incrementValue;
 }
+
+let sliderElement = document.querySelector("#slider");
+let sliderText = document.querySelector("#slider-value");
+
+sliderElement.addEventListener("input", () => {
+  incrementValue = parseFloat(sliderElement.value);
+  sliderText.textContent = incrementValue;
+});
