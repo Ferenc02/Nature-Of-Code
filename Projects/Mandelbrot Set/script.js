b21c371f1dc1183391afb2983a198e3f274484c5
ferenc02 <ferenc.sziraki@medieinstitutet.se>
2024-12-11 21:28:02 +0100

    :sparkles: Add Mandelbrot Set project with initial HTML structure and basic canvas setup; include styling and script for rendering

---
+++ Projects/Mandelbrot Set/script.js
@@ -0,0 +1,13 @@
+let canvasWidth = 600;
+let canvasHeight = 600;
+
+function setup() {
+  let canvas = createCanvas(canvasWidth, canvasHeight);
+  canvas.parent("canvas");
+}
+
+function draw() {
+  background(50);
+  fill(255);
+  ellipse(width / 2, height / 2, 50, 50);
+}
